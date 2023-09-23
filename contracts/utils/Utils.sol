88f1d343ef5bd044378ebb34f02551e3cabbf703
solidity12 <volibear1001@gmail.com>
2023-09-23 23:26:18 +0900

    build: Add Util contract

---
+++ contracts/utils/Utils.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity =0.5.16;
+
+import "../UniswapV2Pair.sol";
+
+contract Utils {
+    function initcodehash() external pure returns (bytes32 bytecode) {
+        bytecode = keccak256(type(UniswapV2Pair).creationCode);
+    }
+}
